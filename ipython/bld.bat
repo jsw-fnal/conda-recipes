2654f9fb383f808f2904ac69f183c42f0b2d48ad
Jon Wilson <jsw@fnal.gov>
2015-02-28 15:36:05 -0800

    added recipes for ipython, jsonschema, and mistune

--- ipython/bld.bat
+++ ipython/bld.bat
@@ -1,8 +1,7 @@
 copy %RECIPE_DIR%\IPython.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
-python setup.py install
-if errorlevel 1 exit 1
+:: Add more build steps here, if they are necessary.
 
 rd /s /q %SCRIPTS%
 rd /s /q %SP_DIR%\ipython-%PKG_VERSION%-py%PY_VER%.egg\share
